60603275f2407c3f2c937772049c7b95c27804de
Ayush Nigam <ayushnigam@Ayushs-MacBook-Air.local>
2022-01-12 23:52:43 +0530

    Updates

---
+++ precache-manifest.914d6c298fa6a3c5111b04f38277cc3f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b185b845c4204396f5d45595c9dce8",
+    "url": "/weather-app-js-app/index.html"
+  },
+  {
+    "revision": "9338008731f185b48bb8",
+    "url": "/weather-app-js-app/static/css/2.66d1a863.chunk.css"
+  },
+  {
+    "revision": "d54bc64846a0f81ef144",
+    "url": "/weather-app-js-app/static/css/main.cf839f21.chunk.css"
+  },
+  {
+    "revision": "9338008731f185b48bb8",
+    "url": "/weather-app-js-app/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "d54bc64846a0f81ef144",
+    "url": "/weather-app-js-app/static/js/main.9c0f73ec.chunk.js"
+  },
+  {
+    "revision": "0a1f9e6e4f61c195aa37",
+    "url": "/weather-app-js-app/static/js/runtime~main.5ff2b06a.js"
+  },
+  {
+    "revision": "e1fe4715f9d375df686f2c6e0ada9659",
+    "url": "/weather-app-js-app/static/media/bg.e1fe4715.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app-js-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app-js-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app-js-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app-js-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app-js-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
